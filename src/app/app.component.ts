447ab30f8405fbc2c3560a5145ba7932560ca8a2
Menaf Alici <alicimenaf@medieinstitutet.se>
2020-05-17 18:35:19 +0200

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todoApp';
+  items = [
+    { description: 'Camping', action: 'No' },
+    { description: 'Hiking', action: 'No' },
+    { description: 'Kayaking', action: 'No' },
+  ];
 }
